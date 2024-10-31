1fdafd878f04c137b1f1c17e8bb079b1c39d1f8d
n0rbed <yassinahmad789@gmail.com>
2024-10-31 15:43:45 +0300

    quick fixes

--- inlabdbproj/Pages/Browse.cshtml.cs
+++ inlabdbproj/Pages/Browse.cshtml.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection.Metadata.Ecma335;
 
 namespace inlabdbproj.Pages
 {
     public class BrowseModel : PageModel
     {
+        public bool exists { get; set; }
+
         public void OnGet()
         {
+            exists = string.IsNullOrEmpty(HttpContext.Session.GetString("username"));
+            
         }
     }
 }
